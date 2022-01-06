cf751ef5076e7944a3d7f9820805159feb199827
Unknown <neeam.radia@gmail.com>
2022-01-06 00:41:58 +0000

    Messed Around with LIN ADVANCE

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -959,7 +959,7 @@
   #endif
 #endif
 
-#define DEFAULT_EJERK    5.0  // May be used by Linear Advance
+#define DEFAULT_EJERK    10.0  // May be used by Linear Advance
 
 /**
  * Junction Deviation Factor
@@ -982,7 +982,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-#define S_CURVE_ACCELERATION
+//#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
