fa77ded491459fed96688b941b49a8eb04813663
Neeam Radia <neeam.radia@gmail.com>
2022-02-28 22:19:29 +0000

    UPDATES AFTERMODS

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -460,7 +460,7 @@
  *   998 : Dummy Table that ALWAYS reads 25°C or the temperature defined below.
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  */
-#define TEMP_SENSOR_0 1
+#define TEMP_SENSOR_0 5
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
@@ -1338,8 +1338,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 300 //310
-#define Y_BED_SIZE 310 //320
+#define X_BED_SIZE 310 //310
+#define Y_BED_SIZE 320 //320
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
